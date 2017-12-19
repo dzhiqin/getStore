9c5a3a3fa53052afc60ad0a9e275fc79a50b281d
dzhiqin <dzhiqin@126.com>
2017-12-19 21:24:11 +0800

    实作添加到购物车的功能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,5 +6,14 @@ class ProductsController < ApplicationController
   def show
     @product=Product.find(params[:id])
   end
-
+  def add_to_cart
+    @product=Product.find(params[:id])
+    if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice]="add to cart success"
+    else
+      flash[:alert]="already added"
+    end
+    redirect_to :back
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   mount_uploader :product_img, ProductImgUploader
+  has_many :cart_items
+  
 end

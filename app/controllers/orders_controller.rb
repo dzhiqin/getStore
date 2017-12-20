002691104eb3c7479a5ab72922f2a4ea0e40602d
dzhiqin <dzhiqin@126.com>
2017-12-20 10:16:40 +0800

    完成订单明细界面

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,15 @@ class OrdersController < ApplicationController
     @order.user=current_user
     @order.total=current_cart.total_price
     if @order.save
+      current_cart.cart_items.each do |cart_item|
+        product_list=ProductList.new
+        product_list.order=@order
+        product_list.product_name=cart_item.product.title
+        product_list.product_price=cart_item.product.price
+        product_list.quantity=cart_item.quantity
+        product_list.save
+      end
+      current_cart.clean!
       redirect_to order_path(@order)
     else
       render "carts/checkout"
@@ -12,6 +21,7 @@ class OrdersController < ApplicationController
   end
   def show
     @order=Order.find(params[:id])
+    @product_lists=@order.product_lists
   end
   private
   def order_params

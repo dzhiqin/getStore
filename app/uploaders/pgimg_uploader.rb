0e61862285c3d1e8f41144d7a8d91d0582a1321b
dzhiqin <dzhiqin@126.com>
2018-01-10 00:30:28 +0800

    pgimg上传七牛云

--- app/uploaders/pgimg_uploader.rb
+++ app/uploaders/pgimg_uploader.rb
@@ -10,7 +10,12 @@ class PgimgUploader < CarrierWave::Uploader::Base
     process resize_to_fit:[400,400]
   end
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
+  if Rails.env.development?
+     storage :file
+   else
+     storage :qiniu
+   end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.

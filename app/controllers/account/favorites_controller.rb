65d04c2361a2d492c1e04ea8a1e26dbc8f90d20e
dzhiqin <dzhiqin@126.com>
2018-01-10 10:51:02 +0800

    订单和收藏添加删除功能

--- app/controllers/account/favorites_controller.rb
+++ app/controllers/account/favorites_controller.rb
@@ -1,6 +1,8 @@
 class Account::FavoritesController < ApplicationController
   before_action :authenticate_user!
+  layout "mylayout"
   def index
     @products=current_user.favorite_products
   end
+  
 end
--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -1,7 +1,13 @@
 class Account::OrdersController < ApplicationController
   before_action :authenticate_user!
+  layout "mylayout"
   def index
     @orders=current_user.orders.order("id DESC")
   end
-  
+  def destroy
+    @order=Order.find_by_token(params[:id])
+    @order.destroy
+    redirect_to account_orders_path
+    flash[:alert]="成功删除订单"
+  end
 end
